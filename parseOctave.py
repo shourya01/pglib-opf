dd645fcd5cb5f9b6c54c6b21dc22c0907fbfafc7
exx <exx@nlp-in-477-l.soe.ucsc.edu>
2024-04-14 00:03:08 -0700

    Squashed commit of the following:
    commit eed01a4b46580f99544d3026ed1f145796f9ed61
    Author: exx <exx@nlp-in-477-l.soe.ucsc.edu>
    Date:   Sun Apr 14 00:01:09 2024 -0700
    
        add all cases
    
    commit e8ce59a3a2f7027f728920e21746eae086937e8c
    Author: exx <exx@nlp-in-477-l.soe.ucsc.edu>
    Date:   Sun Apr 14 00:00:09 2024 -0700
    
        add all cases
    
    commit 04018a97e07ea7bdaf3d460fe8de23d7153f5686
    Author: exx <exx@nlp-in-477-l.soe.ucsc.edu>
    Date:   Sat Apr 13 23:15:29 2024 -0700
    
        add all cases

---
+++ parseOctave.py
@@ -0,0 +1,53 @@
+import os, pickle
+import numpy as np
+from oct2py import Oct2Py
+import cyipopt
+import warnings
+from tqdm import trange
+from pypower.ext2int import ext2int
+from pypower.api import loadcase
+from problemDefJITM import opfSocp
+from problemDefJITMargin import opfSocpMargin
+
+# get octave object
+octave = Oct2Py()
+# filter warnings
+warnings.simplefilter("ignore", np.ComplexWarning)
+
+# user options 
+MAX_BUS = 10000 # upper limit of number of buses in cases to be considered
+
+if __name__ == "__main__":
+    
+    # get all cases in current directory
+    current_directory = os.getcwd()+'/'
+    # current_directory = '/home/sbose/pglib-opf/' # for running on BEBOP
+    all_files_and_directories = os.listdir(current_directory)
+    # case_files = [os.path.join(current_directory, f) for f in all_files_and_directories if f.endswith('.m') and os.path.isfile(os.path.join(current_directory, f))]
+    case_files = case_files = [current_directory+i for i in ['pglib_opf_case118_ieee.m',"pglib_opf_case10000_goc.m"]] # case specific
+
+    cases, casenames = [], []
+    cases_full, casenames_full = [], []
+    for cf in case_files:
+        octave.source(current_directory+os.path.basename(cf))
+        cname = os.path.basename(cf).split('.')[0]
+        num_buses = None
+        # determine number of buses in the case from its name
+        for ci in cname.split('_'):
+            if 'case' in ci:
+                num_buses = int(''.join([chr for chr in ci.replace('case','',1) if chr.isdigit()]))
+        # fitler out cases with more buses than MAX_BUS
+        if num_buses <= MAX_BUS:
+            # convert to internal indexing
+            case_correct_idx = ext2int(loadcase(octave.feval(cname)))
+            # append
+            cases.append(case_correct_idx)
+            casenames.append(cname)
+        
+    data = {'casenames':casenames,'cases':cases}
+            
+    with open(os.getcwd()+f'/allcases.pkl','wb') as file:
+            pickle.dump(data,file)
+            
+    print("Done!")
+
