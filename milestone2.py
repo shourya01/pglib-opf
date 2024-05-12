15a5853944af300f94b8fff0227ce55487f28e19
exx <exx@nlp-in-477-l.soe.ucsc.edu>
2024-05-12 16:55:42 -0700

    update

--- milestone2.py
+++ milestone2.py
@@ -21,7 +21,7 @@ import cvxpy as cp
 from contextlib import contextmanager
 import warnings
 import cyipopt
-from problemDefJITM import opfSocp
+from problemDefJITM2 import opfSocp
 from problemDefJITMargin import opfSocpMargin
 from copy import deepcopy
 import cProfile
@@ -50,7 +50,7 @@ def suppress_stdout():
             sys.stdout = old_stdout    
 
 # user options 
-MAX_BUS = 4 # upper limit of number of buses in cases to be considered
+MAX_BUS = 40 # upper limit of number of buses in cases to be considered
 RATE = 'A' # which line rating to use ('A','B','C')
 
 if __name__ == "__main__":
@@ -92,49 +92,49 @@ if __name__ == "__main__":
     iterlist = [itm for itm in zip(casenames,cases)]
     for cn,this_case in iterlist:
         
-        # # Initialize
-        # print(f"\n--------\nSolving {cn}.\n--------\n",flush=True)
-        # optObj = opfSocp(this_case,cn)
-        # cub, clb = optObj.calc_cons_bounds()
-        # xub, xlb = optObj.calc_var_bounds()
+        # Initialize
+        print(f"\n--------\nSolving {cn}.\n--------\n",flush=True)
+        optObj = opfSocp(this_case,cn)
+        cub, clb = optObj.calc_cons_bounds()
+        xub, xlb = optObj.calc_var_bounds()
         
-        # # Define IPOPT problem
-        # prob = cyipopt.Problem(
-        #     n = optObj.in_size,
-        #     m = optObj.cons_size,
-        #     problem_obj=optObj,
-        #     lb=xlb,
-        #     ub=xub,
-        #     cl=clb,
-        #     cu=cub
-        # )
+        # Define IPOPT problem
+        prob = cyipopt.Problem(
+            n = optObj.in_size,
+            m = optObj.cons_size,
+            problem_obj=optObj,
+            lb=xlb,
+            ub=xub,
+            cl=clb,
+            cu=cub
+        )
         
-        # # Setup solver options
-        # prob.add_option('tol',1e-6)
-        # prob.add_option('max_iter',2500)
-        # prob.add_option('mumps_mem_percent',25000)
-        # prob.add_option('mu_max',1e-0)
-        # prob.add_option('mu_init',1e-0)
-        # prob.add_option('nlp_lower_bound_inf',-optObj.LARGE_NUMBER+1)
-        # prob.add_option('nlp_upper_bound_inf',optObj.LARGE_NUMBER-1)
-        # prob.add_option('derivative_test','second-order')
+        # Setup solver options
+        prob.add_option('tol',1e-6)
+        prob.add_option('max_iter',2500)
+        prob.add_option('mumps_mem_percent',25000)
+        prob.add_option('mu_max',1e-0)
+        prob.add_option('mu_init',1e-0)
+        prob.add_option('nlp_lower_bound_inf',-optObj.LARGE_NUMBER+1)
+        prob.add_option('nlp_upper_bound_inf',optObj.LARGE_NUMBER-1)
+        prob.add_option('derivative_test','second-order')
         
-        # # Solve ipopt problem
-        # # cProfile.run('x,info = prob.solve(optObj.calc_x0_vanilla())')
-        # x, info = prob.solve(optObj.calc_x0_flatstart())    
+        # Solve ipopt problem
+        # cProfile.run('x,info = prob.solve(optObj.calc_x0_vanilla())')
+        x, info = prob.solve(optObj.calc_x0_flatstart())    
         
-        # # evaluate solution
-        # if info['status_msg'].startswith(b'Algorithm terminated successfully'):
-        #     solved += 1
-        # else:
-        #     unsolved += 1
+        # evaluate solution
+        if info['status_msg'].startswith(b'Algorithm terminated successfully'):
+            solved += 1
+        else:
+            unsolved += 1
             
-        # # Now resolve with warm start
+        # Now resolve with warm start
         
-        # print(f"\n--------\nSolving {cn} with warm start.\n--------\n",flush=True)
+        print(f"\n--------\nSolving {cn} with warm start.\n--------\n",flush=True)
         
-        # prob.add_option('mu_init',1e-7)
-        # prob.solve(info['x'],lagrange=info['mult_g'].tolist(),zl=info['mult_x_L'].tolist(),zu=info['mult_x_U'].tolist())
+        prob.add_option('mu_init',1e-7)
+        prob.solve(info['x'],lagrange=info['mult_g'].tolist(),zl=info['mult_x_L'].tolist(),zu=info['mult_x_U'].tolist())
         
         # print(f"\n--------\nSolving {cn} with warm start and reduced constr.\n--------\n",flush=True)
         
@@ -161,93 +161,93 @@ if __name__ == "__main__":
         
         # prob.solve(info['x'],lagrange=info['mult_g'].tolist(),zl=info['mult_x_L'].tolist(),zu=info['mult_x_U'].tolist())
         
-        # Find min margin
+        # # Find min margin
         
-        print(f"\n--------\nSolving {cn} min margin for pd, qd.\n--------\n",flush=True)
-        optObj = opfSocpMargin(this_case,cn,margin_sense=-1)
-        cub, clb = optObj.calc_cons_bounds()
-        xub, xlb = optObj.calc_var_bounds()
+        # print(f"\n--------\nSolving {cn} min margin for pd, qd.\n--------\n",flush=True)
+        # optObj = opfSocpMargin(this_case,cn,margin_sense=-1)
+        # cub, clb = optObj.calc_cons_bounds()
+        # xub, xlb = optObj.calc_var_bounds()
         
-        print(f"cidx: {optObj.cidx}, vidx: {optObj.vidx}")
+        # print(f"cidx: {optObj.cidx}, vidx: {optObj.vidx}")
         
-        # Define IPOPT problem
-        prob = cyipopt.Problem(
-            n = optObj.in_size,
-            m = optObj.cons_size,
-            problem_obj=optObj,
-            lb=xlb,
-            ub=xub,
-            cl=clb,
-            cu=cub
-        )
+        # # Define IPOPT problem
+        # prob = cyipopt.Problem(
+        #     n = optObj.in_size,
+        #     m = optObj.cons_size,
+        #     problem_obj=optObj,
+        #     lb=xlb,
+        #     ub=xub,
+        #     cl=clb,
+        #     cu=cub
+        # )
         
-        # Setup solver options
-        prob.add_option('tol',1e-6)
-        prob.add_option('max_iter',2500)
-        prob.add_option('mumps_mem_percent',25000)
-        prob.add_option('mu_max',1e-0)
-        prob.add_option('mu_init',1e-0)
-        prob.add_option('nlp_lower_bound_inf',-optObj.LARGE_NUMBER+1)
-        prob.add_option('nlp_upper_bound_inf',optObj.LARGE_NUMBER-1)
-        prob.add_option('derivative_test','second-order')
+        # # Setup solver options
+        # prob.add_option('tol',1e-6)
+        # prob.add_option('max_iter',2500)
+        # prob.add_option('mumps_mem_percent',25000)
+        # prob.add_option('mu_max',1e-0)
+        # prob.add_option('mu_init',1e-0)
+        # prob.add_option('nlp_lower_bound_inf',-optObj.LARGE_NUMBER+1)
+        # prob.add_option('nlp_upper_bound_inf',optObj.LARGE_NUMBER-1)
+        # prob.add_option('derivative_test','second-order')
         
-        # solve
-        x, info = prob.solve(optObj.calc_x0_flatstart())  
-        minMarginPd = x[optObj.vidx['mPd']]
-        minMarginQd = x[optObj.vidx['mQd']]
+        # # solve
+        # x, info = prob.solve(optObj.calc_x0_flatstart())  
+        # minMarginPd = x[optObj.vidx['mPd']]
+        # minMarginQd = x[optObj.vidx['mQd']]
         
-        # Find max margin
+        # # Find max margin
         
-        print(f"\n--------\nSolving {cn} min margin for pd, qd.\n--------\n",flush=True)
-        optObj = opfSocpMargin(this_case,cn,margin_sense=1)
-        cub, clb = optObj.calc_cons_bounds()
-        xub, xlb = optObj.calc_var_bounds()
+        # print(f"\n--------\nSolving {cn} min margin for pd, qd.\n--------\n",flush=True)
+        # optObj = opfSocpMargin(this_case,cn,margin_sense=1)
+        # cub, clb = optObj.calc_cons_bounds()
+        # xub, xlb = optObj.calc_var_bounds()
         
-        # Define IPOPT problem
-        prob = cyipopt.Problem(
-            n = optObj.in_size,
-            m = optObj.cons_size,
-            problem_obj=optObj,
-            lb=xlb,
-            ub=xub,
-            cl=clb,
-            cu=cub
-        )
+        # # Define IPOPT problem
+        # prob = cyipopt.Problem(
+        #     n = optObj.in_size,
+        #     m = optObj.cons_size,
+        #     problem_obj=optObj,
+        #     lb=xlb,
+        #     ub=xub,
+        #     cl=clb,
+        #     cu=cub
+        # )
         
-        # Setup solver options
-        prob.add_option('tol',1e-6)
-        prob.add_option('max_iter',2500)
-        prob.add_option('mumps_mem_percent',25000)
-        prob.add_option('mu_max',1e-0)
-        prob.add_option('mu_init',1e-0)
-        prob.add_option('nlp_lower_bound_inf',-optObj.LARGE_NUMBER+1)
-        prob.add_option('nlp_upper_bound_inf',optObj.LARGE_NUMBER-1)
-        prob.add_option('derivative_test','second-order')
+        # # Setup solver options
+        # prob.add_option('tol',1e-6)
+        # prob.add_option('max_iter',2500)
+        # prob.add_option('mumps_mem_percent',25000)
+        # prob.add_option('mu_max',1e-0)
+        # prob.add_option('mu_init',1e-0)
+        # prob.add_option('nlp_lower_bound_inf',-optObj.LARGE_NUMBER+1)
+        # prob.add_option('nlp_upper_bound_inf',optObj.LARGE_NUMBER-1)
+        # prob.add_option('derivative_test','second-order')
         
-        # solve
-        x, info = prob.solve(optObj.calc_x0_flatstart())  
-        maxMarginPd = x[optObj.vidx['mPd']]
-        maxMarginQd = x[optObj.vidx['mQd']]
+        # # solve
+        # x, info = prob.solve(optObj.calc_x0_flatstart())  
+        # maxMarginPd = x[optObj.vidx['mPd']]
+        # maxMarginQd = x[optObj.vidx['mQd']]
         
-        # plotting
-        pd, qd = optObj.get_loads()
-        fig, axs = plt.subplots(nrows=2,ncols=1,figsize=(10,8))
+        # # plotting
+        # pd, qd = optObj.get_loads()
+        # fig, axs = plt.subplots(nrows=2,ncols=1,figsize=(10,8))
         
-        axs[0].plot(pd+maxMarginPd,'g--',label=r'$P_d^{\max}$')
-        axs[0].plot(pd-minMarginPd,'b-.',label=r'$Q_d^{\min}$')
-        axs[0].plot(pd,'k',linewidth=2,label=r'$P_d$')
-        axs[0].set_xlim([0,pd.size-1])
-        axs[0].set_xlabel('Bus_index')
-        axs[0].set_ylabel('Real demand pu')
-        axs[0].legend()
+        # axs[0].plot(pd+maxMarginPd,'g--',label=r'$P_d^{\max}$')
+        # axs[0].plot(pd-minMarginPd,'b-.',label=r'$Q_d^{\min}$')
+        # axs[0].plot(pd,'k',linewidth=2,label=r'$P_d$')
+        # axs[0].set_xlim([0,pd.size-1])
+        # axs[0].set_xlabel('Bus_index')
+        # axs[0].set_ylabel('Real demand pu')
+        # axs[0].legend()
         
-        axs[1].plot(qd+maxMarginQd,'g--',label=r'$Q_d^{\max}$')
-        axs[1].plot(qd-minMarginQd,'b-.',label=r'$Q_d^{\min}$')
-        axs[1].plot(qd,'k',linewidth=2,label=r'$Q_d$')
-        axs[1].set_xlim([0,pd.size-1])
-        axs[1].set_xlabel('Bus_index')
-        axs[1].set_ylabel('Reactive demand pu')
-        axs[1].legend()
+        # axs[1].plot(qd+maxMarginQd,'g--',label=r'$Q_d^{\max}$')
+        # axs[1].plot(qd-minMarginQd,'b-.',label=r'$Q_d^{\min}$')
+        # axs[1].plot(qd,'k',linewidth=2,label=r'$Q_d$')
+        # axs[1].set_xlim([0,pd.size-1])
+        # axs[1].set_xlabel('Bus_index')
+        # axs[1].set_ylabel('Reactive demand pu')
+        # axs[1].legend()
         
-        plt.savefig(f'{cn}_margins.pdf',format='pdf',bbox_inches='tight')
-        plt.close()
+        # plt.savefig(f'{cn}_margins.pdf',format='pdf',bbox_inches='tight')
+        # plt.close()
