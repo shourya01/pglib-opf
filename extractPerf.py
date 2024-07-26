01142621122f3d3f93754e2ea7114c54df7d265c
exx <exx@nlp-in-477-l.soe.ucsc.edu>
2024-07-25 20:07:09 -0700

    temporarily final

--- extractPerf.py
+++ extractPerf.py
@@ -6,8 +6,9 @@ def extract_numbers_to_latex(input_file, output_file):
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
         for line in infile:
             matches = pattern.findall(line)
-            if len(matches) >= 4:
-                matches = matches[:4]
-                outfile.write(f" & {matches[0]}\\% & {matches[1]}\\% & {matches[2]}\\% & {matches[3]}\\% \\\\\n")
+            matches = [float(m) for m in matches]
+            if len(matches) == 5:
+                outfile.write(f"& {matches[4]:.4f}s & {matches[0]:.2f}\\% & {matches[1]:.2f}\\% & {matches[2]:.2f}\\% & {matches[3]:.2f}\\% & \\\\\n")
+                outfile.write(f"fn: {matches[3]:.6f}, positives: {(matches[0]+matches[3]):.2f}, negatives: {(matches[1]+matches[2]):.2f}\n")
 # Usage
 extract_numbers_to_latex('perf.txt', 'output.txt')
